5338796ef7a5118081f72493818d2e6eaeffa67e
pxai <pello.altadill@gluckgames.com>
2021-02-24 09:28:43 +0100

    Improved help

--- hello/src/app/help/help.component.ts
+++ hello/src/app/help/help.component.ts
@@ -10,11 +10,22 @@ export class HelpComponent implements OnInit {
   private _topic: string = "";
   constructor(private activatedRoute: ActivatedRoute) { }
 
+  get topic ():string {
+      return `${this._topic[0].toUpperCase()}${this._topic.slice(1)}`;
+  }
+
+  get helpText ():string {
+      const helpTexts: any = {
+          "list" : "Help for listing stuff",
+          "about": "Help for about component",
+          "home": "Help about home"
+      };
+      return helpTexts[this._topic];
+  }
+
   ngOnInit(): void {
       this.activatedRoute.params.subscribe(paramsId => {
         this._topic = paramsId.topic;
-        console.log(this._topic);
       });
   }
-
 }
