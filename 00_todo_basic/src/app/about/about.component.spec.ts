5500d220a391796012399d769ebe8609fa5faac9
pxai <pello.altadill@gluckgames.com>
2021-03-09 12:16:18 +0100

    Adds about component

---
+++ 00_todo_basic/src/app/about/about.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AboutComponent } from './about.component';
+
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AboutComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have an About text', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const expected: string = "About ToDo app - Pello Xabier Altadill Izura - http://pello.io";
+    expect(app.p).toEqual(expected);
+  });
+});
