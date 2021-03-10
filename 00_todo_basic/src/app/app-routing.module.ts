4b2622fbb96ed5a508eae507a353f3edfec9dbc8
pxai <pello.altadill@gluckgames.com>
2021-03-10 12:27:32 +0100

    Adds route to component

--- 00_todo_basic/src/app/app-routing.module.ts
+++ 00_todo_basic/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {
+      path: "",
+      component: AppComponent
+  },
+  {
+      path: "about",
+      component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
