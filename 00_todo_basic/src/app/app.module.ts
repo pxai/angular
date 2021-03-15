2223f3f77dda192cbb46ecec69c71e83dca20879
pxai <pello.altadill@gluckgames.com>
2021-03-15 19:00:34 +0100

    Added form

--- 00_todo_basic/src/app/app.module.ts
+++ 00_todo_basic/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    FooterComponent
+    FooterComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 00_todo_basic/src/app/task-form/task-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import Task from '../models/task';
+
+@Component({
+  selector: 'app-task-form',
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.scss']
+})
+export class TaskFormComponent implements OnInit {
+  private _submitted: boolean;
+  private _task: Task;
+  private _priorities: string[];
+  constructor() {}
+
+  ngOnInit(): void {
+    this._priorities = ['low', 'medium', 'high'];
+  }
+
+  get priorities () {
+      return this._priorities;
+  }
+
+  get task () {
+      return this._task;
+  }
+
+  submit() {
+      this._submitted = true;
+  }
+}
